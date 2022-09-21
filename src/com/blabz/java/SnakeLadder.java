419b41d989175c8f5d04ca186760f322db706ab7
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-21 19:12:41 +0530

    [Added] UC-1 starting position of player

--- src/com/blabz/java/SnakeLadder.java
+++ src/com/blabz/java/SnakeLadder.java
@@ -1,9 +1,12 @@
 package com.blabz.java;
 
+import java.util.Random;
+
 public class SnakeLadder {
-			public static void main(String[] args) {
-				int start = 0;
-				System.out.println("Starting position of player is : "+start);
-			}	
+	public static void main(String[] args) {
+		int start = 0;
+		System.out.println("Starting position of player is : " + start);
+
+	}
 
 }
