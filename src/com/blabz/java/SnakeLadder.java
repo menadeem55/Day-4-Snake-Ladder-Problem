f3bccca77d8651428bc726f13094083303a97294
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-21 21:06:45 +0530

    [Added] checking for a option they are number of playing, ladder or Snake

--- src/com/blabz/java/SnakeLadder.java
+++ src/com/blabz/java/SnakeLadder.java
@@ -7,9 +7,26 @@ public class SnakeLadder {
 		int start = 0;
 		System.out.println("Starting position of player is : " + start);
 
-		Random random = new Random(); // random is a container to store the all the random values
-		int dice = random.nextInt(6) + 1; // here the (6+1) is the generating the random value is between the 0 to 6
-		System.out.println("Enter the number: " + dice);
+		Random random = new Random(); 			// random is a container to store the all the random values
+		int dice = random.nextInt(6) + 1; 		// here the (6+1) is the generating the random value is between the 0 to 6
+		System.out.println("Dice result number : " + dice);
+		int userPosition = 0;
+		int cases = random.nextInt(3);
+		switch(cases)
+		{
+		case 0: System.out.println("No Play!!");
+				break;
+		case 1: System.out.println("Ladder!");
+				userPosition = userPosition + dice;
+				break;
+		default: System.out.println("Snake!!");
+				userPosition = userPosition - dice;
+				
+				if(userPosition<0)
+				{ userPosition = 0; 
+				}
+		}
+		System.out.println("Presnt Position of the User: "+userPosition);
 
 	}
 
