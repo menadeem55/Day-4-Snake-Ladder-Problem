81c16793d02051b2eb9212e15cda135f1a78dee8
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-21 18:57:33 +0530

    [Added] Starting position of player

---
+++ src/com/blabz/java/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.blabz.java;
+
+public class SnakeLadder {
+			public static void main(String[] args) {
+				int start = 0;
+				System.out.println("Starting position of player is : "+start);
+			}	
+
+}
